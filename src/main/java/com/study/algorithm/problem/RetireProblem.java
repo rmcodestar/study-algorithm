79dd5a7984aae9da842a4baa2882080ef085d41c
rmcodestar <rmcodestar@naver.com>
2019-05-02 00:59:46 +0900

    [2019.05.01] - https://www.acmicpc.net/problem/14501

--- src/main/java/com/study/algorithm/problem/RetireProblem.java
+++ src/main/java/com/study/algorithm/problem/RetireProblem.java
@@ -7,68 +7,29 @@ public class RetireProblem {
     public static void main(String... arg) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        Task[] tasks = new Task[n + 1];
 
-        int lastIndex = 0;
+        int[] time = new int[n + 2];
+        int[] profit = new int[n + 2];
+
         for (int day = 1; day <= n; day++) {
             int duration = scanner.nextInt();
             int money = scanner.nextInt();
+            int lastDay = day + duration - 1;
 
-            Task task = new Task(day, duration, money);
-
-            if (task.lastDay <= n) {
-                tasks[day - 1] = task;
-                lastIndex = day;
+            if (lastDay <= n) {
+                time[day] = duration;
+                profit[day] = money;
             }
         }
 
-        Map<Integer, Integer> profitMap = new HashMap<>();
-        Map<Integer, Integer> positionMap = new HashMap<>();
-        Map<Integer, Integer> powMap = new HashMap<>();
-
-        for (int i = 1; i < (1 << lastIndex); i++) {
-            int current = Integer.highestOneBit(i);
-            int sub = i & ~current;
-
-            int sum = profitMap.getOrDefault(sub, 0);
-            int to = positionMap.getOrDefault(sub, 0);
-
-            int position = powMap.containsKey(current)? powMap.get(current) : getPowNumber(current, powMap);
-            Task task = tasks[position];
-
-            if (Objects.nonNull(task) && task.startDay > to) {
-                to = task.lastDay;
-                sum += task.money;
-            }
-
-            profitMap.put(i, sum);
-            positionMap.put(i, to);
+        int dp[] = new int[n + 6];
+        int max = 0;
+        for (int index = 1; index <= n + 1; index++) {
+            dp[index] = Math.max(dp[index], max);
+            dp[time[index] + index] = Math.max(dp[time[index] + index], dp[index] + profit[index]);
+            max = Math.max(dp[index], max);
         }
 
-        System.out.println(profitMap.values().stream().max(Integer::compareTo).orElse(0));
-    }
-
-    private static int getPowNumber(int number, Map<Integer, Integer> powMap) {
-        int pow = 0;
-        while((number = number / 2) > 0) {
-            pow ++;
-        }
-
-        powMap.put(number, pow);
-        return pow;
-    }
-
-    public static class Task {
-        public int startDay;
-        public int lastDay;
-        public int duration;
-        public int money;
-
-        public Task(int startDay, int duration, int money) {
-            this.startDay = startDay;
-            this.duration = duration;
-            this.money = money;
-            this.lastDay = startDay + duration - 1;
-        }
+        System.out.println(max);
     }
 }
