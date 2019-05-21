5f5bf61007382f9d89a80f794e37a1ba32c933b1
rmcodestar <rmcodestar@naver.com>
2019-05-22 01:35:52 +0900

    [2019.05.21] 사다리조작 - https://www.acmicpc.net/problem/15684 - nC2, nC3 경우의 수 재귀로 구현

--- src/main/java/com/study/algorithm/problem/FixLadder.java
+++ src/main/java/com/study/algorithm/problem/FixLadder.java
@@ -1,11 +1,10 @@
 package com.study.algorithm.problem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class FixLadder {
+    public static int output = -1;
+
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -23,50 +22,55 @@ public class FixLadder {
 
         }
 
-        if (isCompleted(ladders, new ArrayList<>())) {
+        if (isCompleted(ladders)) {
             System.out.println(0);
             return;
         }
 
         //nC1
-        for (int y = 0; y < ladders.length; y++) {
-            for (int x = 0; x < ladders[0].length; x++) {
-                if (ladders[y][x]) {
-                    continue;
-                }
+        for (int max = 1; max <= 3; max++) {
+            fix(ladders, max, 0, 0);
 
-                if (!(x - 1 >= 0 && ladders[y][x - 1]) || !(x + 1 < ladders[0].length && ladders[y][x + 1])) {
-                    boolean isCompleted = isCompleted(ladders, Arrays.asList(new Position(x, y)));
-
-                    if (isCompleted) {
-                        System.out.println(1);
-                        return;
-                    }
-                }
-
+            if (output != -1) {
+                System.out.println(max);
+                return;
             }
         }
 
-        //nC2
-
-        //nC3
 
         System.out.println(-1);
     }
 
-    public void getPositition(boolean[][] ladders, List<Position> candidates, int count) {
-        if (count == 0) {
+
+    public static void fix(boolean[][] ladders, int max, int current, int index) {
+        if (current >= max) {
+            if (isCompleted(ladders)) {
+                output = max;
+            }
+
             return;
+        }
+
+        int x = index % ladders[0].length;
+        int y = index / ladders[0].length;
+
+        for (int j = y; j < ladders.length; j++) {
+
+            int start = (j == y) ? x : 0;
+            for (int i = start; i < ladders[0].length; i++) {
+                if (ladders[j][i] || (i - 1 >= 0 && ladders[j][i - 1]) || (i + 1 < ladders[0].length && ladders[j][i + 1])) {
+                    continue;
+                }
+
+                ladders[j][i] = true;
+                fix(ladders, max, current + 1, i + j * ladders[0].length);
+                ladders[j][i] = false;
+            }
         }
     }
 
-
-    public static boolean isCompleted(boolean[][] ladders, List<Position> newLadders) {
+    public static boolean isCompleted(boolean[][] ladders) {
         // setup
-        for (Position position : newLadders) {
-            ladders[position.height][position.number] = true;
-        }
-
         int sizeX = ladders[0].length;
         int sizeY = ladders.length;
 
@@ -83,11 +87,6 @@ public class FixLadder {
                 }
             }
         }
-
-        for (Position position : newLadders) {
-            ladders[position.height][position.number] = false;
-        }
-
 
         boolean isCompleted = true;
 
